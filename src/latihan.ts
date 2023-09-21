20ee9507d4798f1865021b4f25c12c6f38628c88
Zainal Abidin <zainalabidinhsc@gmail.com>
2023-09-21 14:02:09 +0700

    learn union & intersection

--- src/latihan.ts
+++ src/latihan.ts
@@ -1,14 +1,39 @@
-interface CatInfo {
-  age: number;
-  breed: string;
+// interface CatInfo {
+//   age: number;
+//   breed: string;
+// }
+
+// type CatName = "miffy" | "boris" | "mordred";
+
+// const cats: Record<CatName, CatInfo> = {
+//   miffy: { age: 10, breed: "Persian" },
+//   boris: { age: 5, breed: "Maine Coon" },
+//   mordred: { age: 16, breed: "British Shorthair" },
+// };
+
+// console.log(cats.mordred);
+
+// Intersection typescript
+interface Book {
+  book_id: number;
+  book_name: string;
 }
 
-type CatName = "miffy" | "boris" | "mordred";
+interface Author {
+  author_id: number;
+  author_name: string;
+}
 
-const cats: Record<CatName, CatInfo> = {
-  miffy: { age: 10, breed: "Persian" },
-  boris: { age: 5, breed: "Maine Coon" },
-  mordred: { age: 16, breed: "British Shorthair" },
+type intersected_type = Book & Author;
+
+let intersected_type_object1: intersected_type = {
+  book_id: 101,
+  book_name: "Typescript is Awesome",
+  author_id: 202,
+  author_name: "Tutorialspoint!",
 };
 
-console.log(cats.mordred);
+console.log("Book Id: " + intersected_type_object1.book_id);
+console.log("Book name: " + intersected_type_object1.book_name);
+console.log("Author Id: " + intersected_type_object1.author_id);
+console.log("Author name: " + intersected_type_object1.author_name);
